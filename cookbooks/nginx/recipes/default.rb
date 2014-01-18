a50ab8687a120b4b22134859ebec81648bdd0f3d
Rob Lee <rob@spartansystems.co>
2014-01-17 19:21:19 -0500

    updates 2

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,66 +1,63 @@
 #
 # Cookbook Name:: nginx
 # Recipe:: default
-# Author:: AJ Christensen <aj@junglist.gen.nz>
 #
-# Copyright 2008-2012, Opscode, Inc.
+# Copyright 2010, Datapipe
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'nginx::ohai_plugin'
+# start off with nginx
+package "nginx"
 
-case node['nginx']['install_method']
-when 'source'
-  include_recipe 'nginx::source'
-when 'package'
-  case node['platform']
-  when 'redhat','centos','scientific','amazon','oracle'
-    if node['nginx']['repo_source'] == 'epel'
-      include_recipe 'yum::epel'
-    elsif node['nginx']['repo_source'] == 'nginx'
-      include_recipe 'nginx::repo'
-    elsif node['nginx']['repo_source'].nil?
-      log "node['nginx']['repo_source'] was not set, no additional yum repositories will be installed." do
-        level :debug
-      end
-    else
-      raise ArgumentError, "Unknown value '#{node['nginx']['repo_source']}' was passed to the nginx cookbook."
-    end
-  end
-  package node['nginx']['package_name']
-  service 'nginx' do
-    supports :status => true, :restart => true, :reload => true
-    action :enable
-  end
-  include_recipe 'nginx::commons'
-end
-
-service 'nginx' do
-  supports :status => true, :restart => true, :reload => true
-  action :start
-end
-
-cookbook_file "readycart.com.crt" do
-  path "/etc/nginx/ssl"
+# setup the document root
+directory node.www.document_root do
+  owner "www-data"
+  group "www-data"
+  mode "0755"
+  recursive true
   action :create
 end
 
-cookbook_file "readycart-wildcard.key" do
-  path "/etc/nginx/ssl"
+directory "/etc/nginx/ssl" do
+  owner "www-data"
+  group "www-data"
+  mode "0755"
+  recursive true
   action :create
 end
 
-apt_package "php5-fpm" do
-  action :install
+# default nginx configuration
+template "/etc/nginx/sites-available/default" do
+  source "nginx-default.erb"
+  variables(
+    :document_root => node.www.document_root,
+    :server_name => node.nginx.server_name
+  )
+end
+
+# nginx.conf, set the processes to total cpu cores
+template "/etc/nginx/nginx.conf" do
+  source "nginx.conf.erb"
+  variables(:processes => node.cpu.total)
+end
+
+# kill apache2 if it's there
+service "apache2" do
+  only_if "pgrep apache2"
+  action [ :disable, :stop ]
+end
+
+# start up nginx
+service "nginx" do
+  supports :status => true, :restart => true
+  action [ :enable, :start ]
+end
+
+# fix some log rotation
+remote_file '/etc/logrotate.d/nginx' do
+  source 'nginx-logrotate'
+  owner 'root'
+  group 'root'
+  mode '644'
 end
